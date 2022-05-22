a04aee3a9589c228884483f1e018e02fb50ff763
DESKTOP-EVJ6QO8\Dmitry <dmitry_zaw@mail.ru>
2022-05-22 21:33:38 +0300

    Lesson 2

---
+++ Tankogeddon/Intermediate/Build/Win64/TankogeddonEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.32.31328
+#include "C:/Games/Epic Games/Unreal Projects/GameProject---Tankogeddon/Tankogeddon/Intermediate/Build/Win64/TankogeddonEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
